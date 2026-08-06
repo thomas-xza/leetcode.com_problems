e61185e971945121dc1c039a8fb1c4171915018b
Thomas <122806804+thomas-xza@users.noreply.github.com>
2026-08-06 23:20:15 +0000

    Tested the AVL implementation at scale (loaded 466550 word full dictionary, was able to find a word within it), added joke on function currying

--- avl_implementation_test.py
+++ avl_implementation_test.py
@@ -6,7 +6,7 @@
 ##  Note that this implementation is not perfect, and does have a few
 ##  quirks, but is approximately of O(logn) size upon use.
 
-import unittest, math, random
+import unittest, math, random, time
 
 from avl_implementation import AVLTree, Node
 
@@ -101,9 +101,60 @@ class TestAVLImplementation(unittest.TestCase):
         ##  Check the height is greater than log2(64).
 
         self.assertGreater(self.t.height,
-                             math.log2(len(self.words)))        
-        
+                             math.log2(len(self.words)))
 
+
+    def test_extensive(self):
+
+        ##  Note that the dictionary takes ~20s to insert to the tree.
+
+        ##  Load file contents to tree.
+
+        str_test = "profanably"
+
+        with open('dictionary.txt', "r") as f:
+            for line in f:
+
+                line_text = line.strip()
+                self.t.insert(line_text)
+
+                if line_text == str_test:
+                    print(f"Adding {str_test}")
+
+        ##  The size of the file is 466550 lines.
+        ##  Check all elements were added successfully.
+
+        self.assertEqual(len(self.t.inorder_traverse()),
+                         466550)
+
+        ##  Check the height is approximately as expected.
+
+        self.assertLessEqual(self.t.height,
+                             math.log2(466550) + 2)
+
+        begin = time.time()
+        
+        ##  Time a lookup (using insert() with a known entry in lieu of a lookup() function).
+
+        res = self.t.insert(str_test)
+
+        print(res)
+
+        ##  Note that a false result from insert() means that an
+        ##  insertion *didn't* happen, which was because the string
+        ##  *was* found.
+
+        ##  This also exemplifies how easy it is to adjust a tree
+        ##  insert() to a lookup() function (and you could probably
+        ##  even do them via function currying etc., in the spirit of
+        ##  libraries by Indian software developers!).
+            
+        if res == False:
+            
+            print(f"Found {str_test}")
+        
+        print(f"Lookup time: f{time.time() - begin}")
+        
         
 if __name__ == '__main__':
     unittest.main()
