3097c7e8a0d10b0c185ff0213f0432b18430aa73
Thomas <122806804+thomas-xza@users.noreply.github.com>
2026-08-08 17:59:29 +0100

    3664 775/844 holy f what a mission

--- 3664_two_letter_card_game.py
+++ 3664_two_letter_card_game.py
@@ -5,7 +5,97 @@ from copy import deepcopy
 
 class Solution:
 
+    def score(self, cards: List[st
+##  This question was hugely misunderstood for a long time... functions have been written and deleted various times in the process.
+
+##  Furthermore, the algorithm did not become clear until various exploration which eventually led to test #728.
+
+class Solution:
+
     def score(self, cards: List[str], x: str) -> int:
+
+        pairs = 0
+
+        ##  Cut all superflous cards.
+        cards = list(filter(lambda card: x in card, cards))
+
+        print(Counter(cards))
+
+        res = True
+        pairs_n = 0
+
+        while res == True and len(cards) > 1:
+
+            res = False
+
+            pair = self.find_two_largest_quantity_pairable(cards, x)
+
+            print("Pair selected", pair)
+
+            if pair is not (None, None):
+                cards, res = self.remove_pair(cards, pair)
+
+            print(cards)
+
+            if res == True:
+                pairs_n += 1
+
+        return pairs_n
+
+
+    def remove_pair(self, cards: list[str], pair: tuple[str]) -> tuple[list[str], int]:
+
+        counts = Counter(cards)
+
+        if counts[pair[0]] > 0 and counts[pair[1]] > 0:
+            cards.remove(pair[0])
+            cards.remove(pair[1])
+            return cards, True
+
+        else:
+            return cards, False
+
+
+    def find_two_largest_quantity_pairable(self, cards: List[str], x: str) -> tuple[int]:
+
+        counts = Counter(cards)
+
+        print(list(counts.items()))
+
+        sorted_counts = sorted(list(counts.items()), key=lambda x: x[1])
+
+        sorted_counts.reverse()
+
+        print("sorted_counts", sorted_counts)
+
+        for i, (card, card_q) in enumerate(sorted_counts):
+
+            card_type = self.classify(card, x)
+
+            for j, (card2, card2_q) in enumerate(sorted_counts):
+
+                card_type2 = self.classify(card2, x)
+
+                if card != card2 and (
+                    (card_type < 2 and card_type2 < 2 and card_type == card_type2) or
+                    (card_type == 2 and card_type2 != 2) or
+                    (card_type != 2 and card_type2 == 2)
+                ):
+
+                    return (card, card2)
+
+        return (None, None)
+
+
+    def classify(self, card: str, x: str) -> int:
+
+        if card[0] == x and card[1] != x:
+            return 0  ## f"{x}_"
+        elif card[1] == x and card[0] != x:
+            return 1  ## f"_{x}"
+        else:
+            return 2  ## f"{x}{x}"
+r], x: str) -> int:
 
         pairs = 0
 
