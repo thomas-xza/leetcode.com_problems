dd3dde635fb9513814d81df147143f0532c257c5
Thomas <122806804+thomas-xza@users.noreply.github.com>
2026-08-10 18:03:00 +0100

    3388 dumping before regressing.

--- 3388_count_beautiful_splits.py
+++ 3388_count_beautiful_splits.py
@@ -1,5 +1,4 @@
 
-##  There is some kind of observation/extrapolation that needs to be made, to progress.
 
 class Solution:
 
@@ -11,26 +10,29 @@ class Solution:
 
         t = len(nums)
 
+        print("t:", t)
+
         beautiful = 0
 
         start = time.time()
 
         nums_new = self.replace_patterns(nums)
+
         total = 0
 
-        char_positions = [i for i in range(1, len(nums))]
+        if nums_new != nums:
 
-        if nums_new == []:
-
-            print("empty_list")
+            print("nums_new!")
             
-            for (a, b) in combinations(char_positions, 2):
+            for (a, b) in combinations([i for i in range(1, len(nums_new))], 2):
 
               if a <= b - a or b - a <= t - b:
 
                 total += 1
 
             return total
+
+        char_positions = [i for i in range(1, len(nums))]
 
         ##print(nums_new)
 
@@ -63,9 +65,9 @@ class Solution:
         
     def replace_patterns(self, nums: List[int]):
 
-        ##  Hardcoding until some pattern among the tests' patterns is known (e.g. do the patterns always start at position 0?).
+        ##  Hardcoding observed patterns until some pattern among the tests' patterns is known (e.g. do the patterns always start at position 0?).
 
-        known_patterns = {"0,1,2,3,4,": ""}
+        known_patterns = {"0,1,2,3,4": "01234"}
 
         num_str = ','.join(str(x) for x in nums)
 
@@ -75,24 +77,23 @@ class Solution:
 
                 num_str_test = num_str.replace(pat, rep)
 
+                num_str_test = num_str_test.split(',')
+
+                print(num_str_test[0], num_str_test)
+
+                num_str_test = list(map(lambda x: int(x), num_str_test))
+
                 print(num_str_test)
 
-                if num_str_test == pat[0:-1]:
+                print(len(num_str_test), len(nums))
+
+                if len(num_str_test) == len(nums) // len(rep):
 
                     print("only pat remained")
 
-                    return []
-
-
+                    return num_str_test
 
         ##print(list(map(lambda x: int(x), num_str.split(','))))
 
         return list(map(lambda x: int(x), num_str.split(',')))
-
-
-        # midpoint = len(nums) // 2
-
-        # for i, _ in range(0, len(nums) // 4):
-
-        #     for j, _ in range(i:len(nums // 4)):
 
